d4720cb7b2016a696b7ee30c64bd4f7cf0fa41d6
Cristian Buitrago <crisis1882@gmail.com>
2025-01-18 15:52:09 -0500

    Paleta de colores e imagenes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'lightBlue': '#C5DDFF',
+      'lighterBlue': '#89A9D9',
+      'MainBlue': '#22224B',
+      'SeccondaryBlue': '#1D3D91',
+      'MainGreen': '#42911D',
+      'MainRed': '#E3504B'
+    },
     extend: {},
   },
   plugins: [],
