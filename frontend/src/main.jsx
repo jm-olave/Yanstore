cf31ad99ec455c25d85532d91ef068b56b5a8097
jm-olave <60241569+jm-olave@users.noreply.github.com>
2025-03-15 23:56:18 -0500

    changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
 import MainMenu from './Pages/MainMenu.jsx'
 import FormLayout from './Layouts/FormLayout.jsx'
