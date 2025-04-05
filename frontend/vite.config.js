653129224251b4d6b9528f3bb5ef0eee20605b5d
jm-olave <60241569+jm-olave@users.noreply.github.com>
2025-04-05 18:14:30 -0500

    check bug with logs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -29,7 +29,7 @@ export default defineConfig(({ command, mode }) => {
         minify: 'terser',
         terserOptions: {
           compress: {
-            drop_console: true,
+            drop_console: false,
             drop_debugger: true,
           },
         },
