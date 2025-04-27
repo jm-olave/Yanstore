55087d969a7373cbfb7a5e7369c85c28462378af
Cristian Buitrago <crisis1882@gmail.com>
2025-04-27 16:49:49 -0500

    Added logo to the main menu

--- frontend/src/Pages/MainMenu.jsx
+++ frontend/src/Pages/MainMenu.jsx
@@ -8,21 +8,7 @@ import PageLayout from '../Layouts/PageLayout'
 const MainMenu = () => {
   return (
     <>
-          {/* <div className='bg-mainBlue pb-8 md:flex-row-reverse md:grid md:grid-cols-2 md:h-screen'>
-            <div className='md:max-w-md xl:max-w-3xl md:col-start-2 md:my-auto'>
-              <YanstoreLogo/>
-            </div>
-            <section className='flex flex-col justify-center text-center m-8 md:items-center md:col-start-1 md:row-start-1'>
-              <h1 className='font-Mulish font-black text-white text-4xl'>Main Menu</h1>
-              <div className='flex flex-col justify-center items-center w-4/5 mt-14 mx-auto gap-14 text-white md:w-full md:gap-8'>
-                <GradientButton url={'add-product'} text={'ADD PRODUCT'}/>
-                <GradientButton url={'add-category'} text={'ADD CATEGORY'}/>
-                <GradientButton url={'add-provider'} text={'ADD PROVIDER'}/>
-                <GradientButton url={'inventory'} text={'INVENTORY'}/>
-                <GradientButton url={'statistics'} text={'STATISTICS'}/>
-              </div>
-            </section>
-          </div> */}
+      <div className=' w-full h-full bg-yanstoreLogo bg-opacity-10 bg-center bg-contain bg-no-repeat'></div>
     </>
   )
 }
