55145943e1e1dda4f0c7227e4e70c5655a7af63b
Cristian Buitrago <crisis1882@gmail.com>
2025-04-27 16:55:25 -0500

    Changing logo size in the main menu

--- frontend/src/Pages/MainMenu.jsx
+++ frontend/src/Pages/MainMenu.jsx
@@ -7,9 +7,9 @@ import PageLayout from '../Layouts/PageLayout'
 
 const MainMenu = () => {
   return (
-    <>
-      <div className=' w-full h-full bg-yanstoreLogo bg-opacity-10 bg-center bg-contain bg-no-repeat'></div>
-    </>
+    <div className='w-full h-full flex items-center justify-center'>
+      <div className=' w-3/4 h-full bg-yanstoreLogo bg-opacity-10 bg-center bg-contain bg-no-repeat'></div>
+    </div>
   )
 }
 
